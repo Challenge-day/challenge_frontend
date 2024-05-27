11c812a99c165e06881c19b3ceb237c1ad42c351
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-05-27 21:56:16 +0200

    style: :art:

--- src/modules/MiningPage/components/MiningPageModule/MiningPageModule.styled.tsx
+++ src/modules/MiningPage/components/MiningPageModule/MiningPageModule.styled.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Container = styled.div`
     border-radius: 25px;
@@ -10,11 +10,20 @@ export const Container = styled.div`
 `;
 
 export const Button = styled.button`
-    background: black;
-    border-radius: 50px;
-    width: 255px;
-    height: 255px;
-    margin-bottom: 40px;
+    padding: 48px 12px;
+    border-radius: 100px;
+    background: var(--white-50, rgba(252, 252, 252, 0.4));
+    box-shadow: 0px 4px 4px 0px rgba(5, 5, 5, 0.25) inset;
+    position: absolute;
+    top: 20%;
+    left: 20%;
+    color: var(--white-100, #fafbfc);
+    text-align: center;
+    font-family: "Prosto One";
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
 `;
 
 export const WrapperButtons = styled.div`
@@ -23,7 +32,7 @@ export const WrapperButtons = styled.div`
 `;
 
 export const ButtonInWrapper = styled.button`
-width: 120px;
+    width: 120px;
     padding: 10px 15px;
     border-radius: 100px;
     border: 1px solid transparent; /* for layout purposes */
@@ -43,4 +52,64 @@ export const BorderForButton = styled.div<{ toLeft?: boolean }>`
             : `linear-gradient(to right, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.1) 100%)`};
     padding: 1px;
     border-radius: 50px;
+`;
+
+export const LoaderContainer = styled.div`
+    position: relative;
+    width: 255px;
+    height: 255px;
+    margin-bottom: 40px;
+`;
+
+export const LoaderText = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 24px;
+    color: #333;
+    white-space: pre;
+    text-align: center;
+`;
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Loader = styled.div`
+    position: relative;
+    border-radius: 50%;
+    width: 270px;
+    height: 270px;
+    box-sizing: border-box;
+
+    &::before {
+        content: '';
+        left: -7px;
+        top: -10px;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><circle cx="50" cy="50" r="45" fill="none" stroke-linecap="round" stroke="rgba(250, 251, 252, 0.20)" stroke-width="4" stroke-dasharray="20, 10, 15, 10, 20, 10, 40, 10, 20, 10" /></svg>');
+    }
+
+    &::after {
+        content: '';
+        left: -7px;
+        top: -10px;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><circle cx="50" cy="50" r="45" fill="none" stroke-linecap="round" stroke="white" stroke-width="4" stroke-dasharray="100" /></svg>');
+        animation: ${spin} 20s linear infinite;
+        clip-path: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><circle cx="50" cy="50" r="45" fill="white" /></svg>');
+    }
+    
 `;
--- src/modules/MiningPage/components/MiningPageModule/MiningPageModule.tsx
+++ src/modules/MiningPage/components/MiningPageModule/MiningPageModule.tsx
@@ -1,15 +1,31 @@
+import { Icon } from "../../../../shared/Icon/Icon";
 import {
     BorderForButton,
     Button,
     ButtonInWrapper,
     Container,
     WrapperButtons,
+    Loader,
+    LoaderText,
+    LoaderContainer,
 } from "./MiningPageModule.styled";
 
 export default function MiningPageModule() {
     return (
         <Container>
-            <Button>START</Button>
+            <LoaderContainer>
+                <Loader />
+                <LoaderText>
+                        <Icon
+                            width={209}
+                            height={209}
+                            iconId={"background-start"}
+                        />
+                    <Button>
+                        START
+                    </Button>
+                </LoaderText>
+            </LoaderContainer>
             <WrapperButtons>
                 <BorderForButton>
                     <ButtonInWrapper>Tasks</ButtonInWrapper>
