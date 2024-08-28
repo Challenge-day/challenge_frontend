cf75a4579a3a466d27b99af1e3626a5043ab61cd
vitayusko <vitayusko@users.noreply.github.com>
2024-08-28 20:40:19 +0200

    Slice, thunk, selector - for task, updated Store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
+// import storage from "redux-persist/lib/storage";
+// import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+// import { serviceReducer } from "./services/servicesSlice";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  PersistConfig,
+} from "redux-persist";
 import { serviceReducer } from "./services/servicesSlice";
+import { taskReducer } from "./task/TaskSlice";
+import IServicesInterface from "./services/servicesInterfaces";
 
-const persistConfig = {
-  key: "root",
+const persistConfig: PersistConfig<IServicesInterface> = {
+  key: "service",
   storage,
   whitelist: ["token"],
 };
 
-const store = configureStore({
-  reducer: {
-    service: persistReducer(persistConfig, serviceReducer),
-  },
+const rootReducer = combineReducers({
+  service: persistReducer(persistConfig, serviceReducer),
+  task: taskReducer,
+});
 
+const store = configureStore({
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
@@ -26,5 +44,31 @@ export const persistor = persistStore(store);
 
 export default store;
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
+
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   whitelist: ["token"],
+// };
+
+// const store = configureStore({
+//   reducer: {
+//     service: persistReducer(persistConfig, serviceReducer),
+//   },
+
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export const persistor = persistStore(store);
+
+// export default store;
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
---
+++ src/redux/task/selectors.ts
@@ -0,0 +1,3 @@
+export const selectTask = (state: any) => state.task.items;
+export const selectLoading = (state: any) => state.task.loading;
+export const selectError = (state: any) => state.task.error;
